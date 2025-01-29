3410485d6868945c03f430ee1b6f318064ab7f22
shreyas <shreyashraut8@gmail.com>
2025-01-29 13:18:15 +0530

    Added Navbar

--- kids_frontend/src/App.jsx
+++ kids_frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+
+import Footer from './components/Footer.jsx';
 import Navbar from './components/Navbar.jsx';
-import Home from './pages/Home.jsx'
-import DentalServices from './pages/DentalServices.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
-import Footer from './components/Footer.jsx';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DentalServices from './pages/DentalServices.jsx';
+import Home from './pages/Home.jsx';
 
 const App = () => {
   return (
-    <div className=''>
-      <Navbar/>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/dentalServices' element={<DentalServices/>} />
-          <Route path='/About' element={<About/>} />
-          <Route path='/Contact' element={<Contact/>} />
-        </Routes>
-      </Router>
-      <Footer/>
-    </div>
-  )
-}
+    <div className="">
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element   ={<Home />} />
+        <Route path="/dentalServices" element={<DentalServices />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
